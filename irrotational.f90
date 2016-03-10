c98cb4cee0a2995fcad419a8eb8e4f10a32e06f2
callumgray <callum.gray.10@ucl.ac.uk>
2016-03-10 11:37:24 +0100

    Initial commit

---
+++ irrotational.f90
@@ -0,0 +1,15 @@
+! Subroutine to find the irrotational part of the lattice field
+! for a cubic lattice
+subroutine linear_solver
+! need to have L declared already
+integer :: a,b,c,x,y,z,kx,ky,kz
+real*8 :: sum_x,sum_y,sum_z,p1,p2,q1,q2,r1,r2
+real*8 :: m1p1,m1p2,m1q1,m1q2,m1r1,m1r2,cosine(L/2-1)
+
+do a=1,L
+  do b=1,L
+    do c=1,L
+      
+
+
+end program irrotational
