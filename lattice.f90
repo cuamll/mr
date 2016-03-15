f55553d9cfa87e2be08ce79391eed3fb5c3b8777
callumgray <callum.gray.10@ucl.ac.uk>
2016-03-15 15:48:58 +0100

    Fix various bugs, various others still outstanding

--- lattice.f90
+++ lattice.f90
@@ -1,3 +1,45 @@
+program config_linsolve
+integer :: L, i, j, k, row, col
+character(len=11) :: filename
+integer, dimension(:,:), allocatable :: v_temp
+integer, dimension(:,:,:), allocatable :: v,mnphi_x,mnphi_y,mnphi_z
+
+! note: getarg(pos, value) does argc/argv
+
+! these parameters will be put in an input file eventually
+filename='lattice.dat'
+
+! read in lattice size (for now) then allocate arrays accordingly
+write(*,*) "Lattice dimension L: "
+read(*,*) L
+allocate(v_temp(L**2,L))
+allocate(v(L,L,L))
+
+! irrotational part of E-field
+allocate(mnphi_x(L,L,L))
+allocate(mnphi_y(L,L,L))
+allocate(mnphi_z(L,L,L))
+
+open(unit=1, file=filename)
+read(1,*)((v_temp(row,col),col=1,L),row=1,L**2)
+
+do i=1,L**2
+  write(*,*) (v_temp(i,j), j=1,L)
+enddo
+
+v = reshape(v_temp, (/L,L,L/))
+deallocate(v_temp) ! we don't need it anymore
+
+do j=1,L
+  do i=1,L
+    write(*,*) (v(i,j,k), k=1,L)
+  enddo
+enddo
+
+call linear_solver
+
+end program config_linsolve
+
 ! Subroutine to find the irrotational part of the lattice field
 ! for a cubic lattice
 
@@ -11,7 +53,7 @@ real*8 :: m1p1,m1p2,m1q1,m1q2,m1r1,m1r2,twopibyL(L/2-1),charge,volume
 
 pi=3.141592653589793
 u_tot=0
-volume=1.0 ! should be a**3, but i have a = 1 anyway
+volume=1.0
 
 call PBCs
 
