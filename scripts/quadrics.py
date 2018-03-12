e61355cfe60570cc51ae697ab4d8cd67831e526e
cuamll <callum.gray.10@ucl.ac.uk>
2018-03-12 16:06:29 +0000

    Create quadric and contour plot scripts, test sans-serif cairolatex option

--- scripts/quadrics.py
+++ scripts/quadrics.py
@@ -2,43 +2,78 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-input_file = 'out/whatever/s_ab_total.dat'
-output_file = 'out/whatever/s_ab_total_eig.dat'
+input_file = 'T_1.650_l64_sweep/s_ab_total.dat'
+s_ab_output_file = 'T_1.650_l64_sweep/s_ab_total_eig.dat'
+chi_output_file = 'T_1.650_l64_sweep/chi_ab_total_eig.dat'
 s_raw = np.loadtxt(input_file)
 
 # we want to separate out the s^{ab} tensors and keep the kvals
 # this is for 2d
 d = 2
+bz = 2
+length = 64
+temperature = 1.650
 kvals = s_raw[:,0:d]
 s_ab_tot = s_raw[:,d:(d*(d+1))]
 # reshape to be a list of dxd matrices
 s_ab_tot = s_ab_tot.reshape((-1,d,d))
+# s_ab_tot = s_ab_tot / ((length**2 * bz**2) + 1
+chi_tot = s_ab_tot / temperature
 
-eigvals = np.zeros((len(s_ab_tot),d))
-eigvecs = np.zeros(s_ab_tot.shape)
+s_ab_inv = np.zeros(s_ab_tot.shape)
+chi_inv = np.zeros(s_ab_tot.shape)
+
+s_ab_eigvals = np.zeros((len(s_ab_tot),d))
+s_ab_eigvecs = np.zeros(s_ab_tot.shape)
+chi_eigvals = np.zeros((len(s_ab_tot),d))
+chi_eigvecs = np.zeros(s_ab_tot.shape)
 
 for i in range(len(s_ab_tot)):
     # following Steve, we want the inverse tensor
     # to ensure none of the principal axes blow up later
-    eigvals[i], eigvecs[i] = np.linalg.eig(np.linalg.inv(s_ab_tot[i]))
+    s_ab_inv[i] = np.linalg.inv(s_ab_tot[i])
+    chi_inv[i] = np.linalg.inv(chi_tot[i])
+    s_ab_eigvals[i], s_ab_eigvecs[i] = np.linalg.eig(np.linalg.inv(s_ab_tot[i]))
+    s_ab_eigvals[i], s_ab_eigvecs[i] = np.linalg.eig(np.linalg.inv(s_ab_tot[i]))
+    chi_eigvals[i], chi_eigvecs[i] = np.linalg.eig(np.linalg.inv(chi_tot[i]))
 
-np.savetxt(output_file, np.concatenate((kvals,eigvals,eigvecs.reshape((-1,d**2))),axis=1))
+np.savetxt(s_ab_output_file, np.concatenate((kvals,s_ab_eigvals,s_ab_eigvecs.reshape((-1,d**2))),axis=1))
+np.savetxt(chi_output_file, np.concatenate((kvals,chi_eigvals,chi_eigvecs.reshape((-1,d**2))),axis=1))
 
 # plotting stuff. this is for 2D
-test = 3000
+test = 4300
+print s_ab_inv[test,:,:]
 
 if d == 2:
-    def quadric(x,y,a,b):
-        return x**2 / a + y**2 / b - 1
+    # def quadric(x, y, a, b):
+    #     return x**2 / a + y**2 / b - 1
+    def quadric(x, y, a, b, c):
+        return x**2 / a + y**2 / b + (2 * x * y) / c - 1
 
-    xlist = np.linspace(-1.0,1.0,200)
-    ylist = np.linspace(-1.0,1.0,200)
+    kv = kvals[test]
+    kv_str = r' $ q = ' + str(kv) + r' $'
+    xlist = np.linspace(-3.0,3.0,400)
+    ylist = np.linspace(-3.0,3.0,400)
     X, Y = np.meshgrid(xlist,ylist)
-    C = quadric(X, Y, eigvals[test,0], eigvals[test,1])
-    plt.contour(X, Y, C, levels=[0])
-    plt.grid()
-    plt.axhline(0, color='black', lw=2)
-    plt.axvline(0, color='black', lw=2)
+    C = quadric(X, Y, s_ab_inv[test,0,0], s_ab_inv[test,1,1], s_ab_inv[test,0,1])
+    C2 = quadric(X, Y, chi_inv[test,0,0], chi_inv[test,1,1], chi_inv[test,0,1])
+    # C = quadric(X, Y, s_ab_eigvals[test,0], s_ab_eigvals[test,1])
+    # C2 = quadric(X, Y, chi_eigvals[test,0], chi_eigvals[test,1])
+    fig, axes = plt.subplots(2, figsize=(4, 10))
+    axes[0].contour(X, Y, C, levels=[0])
+    axes[0].grid()
+    axes[0].arrow(0.0,0.0,s_ab_eigvecs[test,0,0],s_ab_eigvecs[test,1,0],color='green')
+    axes[0].arrow(0.0,0.0,s_ab_eigvecs[test,0,1],s_ab_eigvecs[test,1,1],color='green')
+    axes[0].axhline(0, color='black', lw=2)
+    axes[0].axvline(0, color='black', lw=2)
+    axes[0].set_title(r'$ S^{\alpha\beta}_{tot} $ quadric, ' + kv_str)
+    axes[1].contour(X, Y, C2, levels=[0])
+    axes[1].grid()
+    axes[1].arrow(0.0,0.0,chi_eigvecs[test,0,0],chi_eigvecs[test,0,1],color='green')
+    axes[1].arrow(0.0,0.0,chi_eigvecs[test,1,0],chi_eigvecs[test,1,1],color='green')
+    axes[1].axhline(0, color='black', lw=2)
+    axes[1].axvline(0, color='black', lw=2)
+    axes[1].set_title(r'$ \chi^{\alpha\beta}_{tot} $ quadric, ' + kv_str)
     # if you wanna see it, I guess
     plt.show()
 
--- scripts/s_perp_contours.py
+++ scripts/s_perp_contours.py
@@ -2,71 +2,115 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as tck
 import colormaps as cm
+# currently stuck with system python 2.7 so ugly mkdir
+import errno
+import os
 
-dir = 'T_1.650_l64_sweep/'
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+indir = 'T_1.525_l64_sweep/'
 filename = 's_perp_total'
 suffix = '.dat'
-input_file = dir + filename + suffix
+input_file = indir + filename + suffix
+outdir = indir + 'contour_plots/'
+mkdir_p(outdir)
+# os.makedirs(outdir, exist_ok=True) # python > 3.2
 d = 2
 length = 64
 sp = 8
-i = 2
-j = 2
-output_file = dir + 'gx_' + str(i) + '_gy_' + str(j) + filename + '.png'
 s_p_raw = np.loadtxt(input_file)
 kvals = s_p_raw[:,0:d]
-xlist = np.linspace(kvals[0,0],kvals[-1,-1],np.sqrt(len(kvals)))
-ylist = np.linspace(kvals[0,0],kvals[-1,-1],np.sqrt(len(kvals)))
-X, Y = np.meshgrid(xlist, ylist)
+kvals = np.unique(kvals)
 intens = s_p_raw[:,d:d+1]
 intens = intens.reshape((np.sqrt(len(intens)),np.sqrt(len(intens))))
 
 def g_to_index(x,y):
     return (((sp + 2*x) * (length / 2)),((sp + 2*y) * (length / 2)))
 
+# def s_p(x, y, G_x, G_y):
+#     if G_x == x and G_y == y:
+#         return 0.0
+#     else:
+#         return (((G_x - x)*x + (G_y - y)*y)**2)/((x**2 + y**2)*((x - G_x)**2 + (y - G_y)**2))
+
 def s_p(x, y, G_x, G_y):
     return (((G_x - x)*x + (G_y - y)*y)**2)/((x**2 + y**2)*((x - G_x)**2 + (y - G_y)**2))
 
-gx, gy = g_to_index(i,j)
-qx = xlist[gx-length/2:gx+1+length/2]
-qy = ylist[gy-length/2:gy+1+length/2]
-Qx, Qy = np.meshgrid(qx, qy)
-sim_int = intens[gx-length/2:gx+1+length/2, gy-length/2:gy+1+length/2]
-ana_int = s_p(Qx, Qy, qx[length/2], qx[length/2])
-# central point will go to 0/0 for this expression; remove resulting NaN
-ana_int[np.isnan(ana_int)] = 0.0
-# check the ratios
-quot = sim_int / ana_int
-quot[np.isinf(quot)] = 0.0
+# we want it empty and zero-length, so we can append to it in the loop
+tot_ana_int = np.array([])
 
-# this is v. ugly but was the easiest way to get decent-looking tics
-# and colour bars that match the sizes of the subplots
-fig, axes = plt.subplots(ncols=3, nrows=1, figsize=(12,4))
-ax = axes[0]
-ax.xaxis.set_major_formatter(tck.FormatStrFormatter('%g $\pi$'))
-ax.xaxis.set_major_locator(tck.MultipleLocator(base=1.0))
-ax.yaxis.set_major_formatter(tck.FormatStrFormatter('%g $\pi$'))
-ax.yaxis.set_major_locator(tck.MultipleLocator(base=1.0))
-ax.set_title('Simulated $ S_{\perp} $')
-cs = ax.contourf(Qx / np.pi, Qy / np.pi, sim_int, cmap=cm.viridis)
-fig.colorbar(cs, ax=ax)
+for i in range(-1*(sp/2) + 1, (sp/2)):
+    for j in range(-1*(sp/2) + 1, (sp/2)):
 
-ax = axes[1]
-ax.xaxis.set_major_formatter(tck.FormatStrFormatter('%g $\pi$'))
-ax.xaxis.set_major_locator(tck.MultipleLocator(base=1.0))
-ax.yaxis.set_major_formatter(tck.FormatStrFormatter('%g $\pi$'))
-ax.yaxis.set_major_locator(tck.MultipleLocator(base=1.0))
-ax.set_title('Analytical $ S_{\perp} $')
-cs = ax.contourf(Qx / np.pi, Qy / np.pi, ana_int, cmap=cm.viridis)
-fig.colorbar(cs, ax=ax)
+        output_file = outdir + 'gx_' + str(i) + '_gy_' + str(j) + '_' + filename + '.png'
+        gx, gy = g_to_index(i,j)
+        qx = kvals[gx-length/2:gx+1+length/2]
+        qy = kvals[gy-length/2:gy+1+length/2]
+        #ana_int = np.zeros((len(qx),len(qy)))
+        Qx, Qy = np.meshgrid(qx, qy)
+        sim_int = intens[gx-length/2:gx+1+length/2, gy-length/2:gy+1+length/2]
+        # sim_int = intens[gx+1+length/2:gx-length/2:-1, gy-length/2:gy+1+length/2]
+        ana_int = s_p(Qx, Qy, qx[length/2], qy[length/2])
 
-ax = axes[2]
-ax.xaxis.set_major_formatter(tck.FormatStrFormatter('%g $\pi$'))
-ax.xaxis.set_major_locator(tck.MultipleLocator(base=1.0))
-ax.yaxis.set_major_formatter(tck.FormatStrFormatter('%g $\pi$'))
-ax.yaxis.set_major_locator(tck.MultipleLocator(base=1.0))
-ax.set_title('Quotient $ S_{\perp}^{simulated} / S_{\perp}^{analytic} $')
-cs = ax.contourf(Qx / np.pi, Qy / np.pi, quot, cmap=cm.viridis)
-fig.colorbar(cs, ax=ax)
+        # for k in range(len(qx)):
+        #     for m in range(len(qy)):
+        #         ana_int[k,m] = s_p(qx[k], qy[m], qx[length/2], qy[length/2])
 
-fig.savefig(output_file)
+        # for some reason sim_int comes out transposed?
+        # ana_int = ana_int.T
+        # sim_int = sim_int.T
+        # central point will go to 0/0 for this expression; remove resulting NaN
+        ana_int[np.isnan(ana_int)] = 0.0
+        tot_ana_int = np.append(tot_ana_int, ana_int.flatten())
+
+        # check the ratios
+        quot = sim_int / ana_int
+        quot[np.isinf(quot)] = 0.0
+
+        # this is v. ugly but was the easiest way to get decent-looking tics
+        # and colour bars that match the sizes of the subplots
+        fig, axes = plt.subplots(ncols=3, nrows=1, figsize=(15,4))
+        ax = axes[0]
+        ax.xaxis.set_major_formatter(tck.FormatStrFormatter('%g $\pi$'))
+        ax.xaxis.set_major_locator(tck.MultipleLocator(base=1.0))
+        ax.yaxis.set_major_formatter(tck.FormatStrFormatter('%g $\pi$'))
+        ax.yaxis.set_major_locator(tck.MultipleLocator(base=1.0))
+        ax.set_title('Simulated $ S_{\perp} $')
+        cs = ax.contourf(Qx / np.pi, Qy / np.pi, sim_int, cmap=cm.viridis)
+        fig.colorbar(cs, ax=ax)
+
+        ax = axes[1]
+        ax.xaxis.set_major_formatter(tck.FormatStrFormatter('%g $\pi$'))
+        ax.xaxis.set_major_locator(tck.MultipleLocator(base=1.0))
+        ax.yaxis.set_major_formatter(tck.FormatStrFormatter('%g $\pi$'))
+        ax.yaxis.set_major_locator(tck.MultipleLocator(base=1.0))
+        ax.set_title('Analytical $ S_{\perp} $')
+        cs = ax.contourf(Qx / np.pi, Qy / np.pi, ana_int, cmap=cm.viridis)
+        fig.colorbar(cs, ax=ax)
+
+        ax = axes[2]
+        ax.xaxis.set_major_formatter(tck.FormatStrFormatter('%g $\pi$'))
+        ax.xaxis.set_major_locator(tck.MultipleLocator(base=1.0))
+        ax.yaxis.set_major_formatter(tck.FormatStrFormatter('%g $\pi$'))
+        ax.yaxis.set_major_locator(tck.MultipleLocator(base=1.0))
+        ax.set_title('Quotient $ S_{\perp}^{simulated} / S_{\perp}^{analytic} $')
+        cs = ax.contourf(Qx / np.pi, Qy / np.pi, quot, cmap=cm.viridis)
+        fig.colorbar(cs, ax=ax)
+
+        fig.tight_layout()
+        fig.savefig(output_file)
+        plt.close(fig)
+
+# we can print out tot_ana_int and inspect it like s_perp_total
+# just can't figure out the syntax rn
+tot_ana_int = tot_ana_int.reshape((np.sqrt(len(tot_ana_int)),np.sqrt(len(tot_ana_int))))
+fig, axes = plt.subplots()
+axes.imshow(tot_ana_int, interpolation = 'bicubic', cmap=cm.viridis)
+plt.show()
